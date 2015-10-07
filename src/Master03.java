b4f71203014d0ee7541ac623c6746d2bcff09aac
emoralm <emoramo@gmail.com>
2015-10-07 16:24:48 +0200

    Commit 02/10 16:24

--- src/Master03.java
+++ src/Master03.java
@@ -4,6 +4,7 @@ public class Master03 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Codigo de la clasee master03");
+		// modificado
 	}
 
 }
