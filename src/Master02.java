0347372c7c6e1a9a8fbd9d87faf5918cc38d9e35
emoralm <emoramo@gmail.com>
2015-10-11 20:28:19 +0200

    ccccccc

--- src/Master02.java
+++ src/Master02.java
@@ -4,11 +4,8 @@ public class Master02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Prueba de la clase Master02");
-		// comentario desde eclipse
 		//mdoficado en GitHub
-		// cambiado tras los cambios
-		
-		
+		//modfiicado con Git
 	}
 
 }
