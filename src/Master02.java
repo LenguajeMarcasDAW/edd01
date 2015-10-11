c1b99d900c985093aa0e75d017c82f42d8fe05f8
emoralm <emoramo@gmail.com>
2015-10-11 21:20:03 +0200

    dddddddd

--- src/Master02.java
+++ src/Master02.java
@@ -4,13 +4,8 @@ public class Master02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Prueba de la clase Master02");
-		
 		// cambio por lo que quiero
 		// vuelvo a pisar
-		// cargo modificación Eclipse
-		// guardo
-		// vuelvo a modificar
-		
 
 	}
 
