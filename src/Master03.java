d5c9021b3df01c8749dc0f4ca5bcc66393ed5f18
emoralm <emoramo@gmail.com>
2015-10-11 17:20:29 +0200

    eeee

--- src/Master03.java
+++ src/Master03.java
@@ -4,7 +4,7 @@ public class Master03 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Codigo de la clasee master03");
-		
+		// modifico en master y voy a mezclar
 		
 		
 	}
