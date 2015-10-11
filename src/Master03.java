fbc4918017622a9759e0d7e81357b2e3b5d07bc9
emoralm <emoramo@gmail.com>
2015-10-11 18:53:17 +0200

    Commit reseteado

--- src/Master03.java
+++ src/Master03.java
@@ -5,9 +5,15 @@ public class Master03 {
 		// TODO Auto-generated method stub
 		System.out.print("Codigo de la clasee master03");
 		// modifico en master y voy a mezclar
+
 		// modifico en rama master despues de cambiar en rama01
 		// compruebado
 		// nueva linea
+
+		// modifico en rama01
+		// vuelvo a cambiar
+		// problemas en rama
+
 		
 		}	
 	}
