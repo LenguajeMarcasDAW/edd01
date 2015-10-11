3805d5666737ebed4d2ab31d71f54ae7d7d1dfb8
LenguajeMarcasDAW <emmcuenta@gmail.com>
2015-10-11 20:59:06 +0200

    Commit 20:59

--- src/Master02.java
+++ src/Master02.java
@@ -4,8 +4,8 @@ public class Master02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Prueba de la clase Master02");
-		//mdoficado en GitHub
-		// modifico en codigo
+		// cambio por lo que quiero
+		// vuelvo a pisar
 
 	}
 
