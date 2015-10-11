420edfe09b1e099363e2bee92cdbb67591dd42f4
emoralm <emoramo@gmail.com>
2015-10-11 17:15:55 +0200

    Commit antes de rama

--- src/Master03.java
+++ src/Master03.java
@@ -4,9 +4,7 @@ public class Master03 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Codigo de la clasee master03");
-		// Modificado desde GitHUb
-		// Modificado desde Eclipase
-		// fichero modificado desde el navegador.
+		
 		
 		
 	}
